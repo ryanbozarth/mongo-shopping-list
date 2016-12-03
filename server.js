0db74ad8476043bf08b701c37af85ced3f2e4350
Ryan Bozarth <ryan.bozarth@gmail.com>
2016-12-03 08:47:57 -0800

    created update anddelete methods to mongo

--- server.js
+++ server.js
@@ -10,26 +10,26 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 var runServer = function(callback) {
-    mongoose.connect(config.DATABASE_URL, function(err) {
-        if (err && callback) {
-            return callback(err);
-        }
+  mongoose.connect(config.DATABASE_URL, function(err) {
+    if (err && callback) {
+      return callback(err);
+    }
 
-        app.listen(config.PORT, function() {
-            console.log('Listening on localhost:' + config.PORT);
-            if (callback) {
-                callback();
-            }
-        });
+    app.listen(config.PORT, function() {
+      console.log('Listening on localhost:' + config.PORT);
+      if (callback) {
+        callback();
+      }
     });
+  });
 };
 
 if (require.main === module) {
-    runServer(function(err) {
-        if (err) {
-            console.error(err);
-        }
-    });
+  runServer(function(err) {
+    if (err) {
+      console.error(err);
+    }
+  });
 };
 
 exports.app = app;
@@ -38,31 +38,57 @@ exports.runServer = runServer;
 var Item = require('./models/item');
 
 app.get('/items', function(req, res) {
-    Item.find(function(err, items) {
-        if (err) {
-            return res.status(500).json({
-                message: 'Internal Server Error'
-            });
-        }
-        res.json(items);
-    });
+  Item.find(function(err, items) {
+    if (err) {
+      return res.status(500).json({
+        message: 'Internal Server Error'
+      });
+    }
+    res.json(items);
+  });
 });
 
 app.post('/items', function(req, res) {
-    Item.create({
-        name: req.body.name
-    }, function(err, item) {
-        if (err) {
-            return res.status(500).json({
-                message: 'Internal Server Error'
-            });
-        }
-        res.status(201).json(item);
+  Item.create({
+    name: req.body.name
+  }, function(err, item) {
+    if (err) {
+      return res.status(500).json({
+        message: 'Internal Server Error'
+      });
+    }
+    res.status(201).json(item);
+  });
+});
+
+app.put('/items/:id', function(req, res) {
+  Item.findByIdAndUpdate(req.params.id, {
+    name: req.body.name
+  }, function(err, item) {
+    if (err) {
+      return res.status(400).json({
+        message: 'Error'
+      });
+    }
+    res.status(200).json(item);
+  });
+});
+
+app.delete('/items/:id', function(req, res) {
+  Item.findByIdAndRemove(
+    req.params.id,
+    function(err, item) {
+      if (err) {
+        return res.status(400).json({
+          message: 'Error'
+        });
+      }
+      res.status(200).json(item);
     });
 });
 
 app.use('*', function(req, res) {
-    res.status(404).json({
-        message: 'Not Found'
-    });
+  res.status(404).json({
+    message: 'Not Found'
+  });
 });
